775ac75aa7c2ac5f64f6c38bb092cfd041eaa448
Tbjorkman <tabithabjorkman@gmail.com>
2018-12-15 00:59:55 +0100

    search results are mostly done

--- js/script.js
+++ js/script.js
@@ -1,40 +1,33 @@
 
 /******************** Fetching *********************/
 
-//  "use strict"
+  "use strict"
 
-// function getData(){
-//     fetch("http://tabithabjorkman.com/bifrost_t/json/categories.php")
-//     fetch("http://tabithabjorkman.com/bifrost_t/json/skills.php")
-//     .then(res => res.json())
-//     //.then(showCategories);
-//     .then(showSkills);
+ function getData(){
+     fetch("http://tabithabjorkman.com/bifrost_t/json/students.php")
+     .then(res => res.json())
+     //.then(showCategories);
+     .then(showPersonsIcons);
    
-// }
+ }
 
-// let SSList = document.querySelector("#SoftSkills");
-// let SSTemplate = document.querySelector("#SSTemplate").content;
-// //const catTemp = document.querySelector (".categoryTemplate").content; 
+  let searchResult = document.querySelector("#searchResults");
+  let SearchResultTemplate = document.querySelector("#SearchResultTemplate").content;
 
-// function showSkills(data){
-//     console.log(data);
-//     data.forEach(function (theSkill) {    
-//              //console.log('hi there');
-//              console.log(theSkill.categories_category_id);
-//         const clone = SSTemplate.cloneNode(true);
-//         let cat_id_skill = 
-//         //console.log('hi clone');
-//         const name = clone.querySelector(".ItemSoftSkill");
-//         clone.querySelector(".ItemSoftSkill").textContent = theSkill.categories_category_id[1];
-
-//         //console.log('this is a loop');
-      
-//         SSList.appendChild(clone);
+  function showPersonsIcons(persData){
+     console.log(persData);
+     persData.forEach(function (thePers) {  
+              console.log(thePers.student_id.length);
+         const clone = SearchResultTemplate.cloneNode(true);
+         //console.log('hi clone');
+         clone.querySelector(".persIconImg").textContent = thePers.student_id;
+         //console.log('this is a loop');
+         searchResult.appendChild(clone);
         
-//     });
-// }
+    });
+}
 
-// getData(); 
+getData(); 
 
 /******************** Fetching Skills & Cloning it to tabs *********************/
  "use strict"
@@ -82,7 +75,6 @@ function showSkillsSoft(data){
         if(cat_id_skill == 1)
         {
           clone.querySelector(".ItemSoftSkill").textContent = theSkill.skills;
-          
         }
         SSList.appendChild(clone);
     });
