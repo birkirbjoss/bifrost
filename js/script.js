f80c2bd868bcb1c88285b2088baa0a42e7658aca
Tbjorkman <tabithabjorkman@gmail.com>
2018-12-17 16:36:42 +0100

    fuction deleted and added to search_results.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,4 @@
 
-/******************** Fetching *********************/
-
-  
-
-   function getDataResults(){
-     fetch("http://tabithabjorkman.com/bifrost_t/json/students.php")
-     .then(res => res.json())
-     //.then(showCategories);
-     .then(showPersonsIcons);
-   
- }
-
- let searchResult = document.querySelector("#searchResults");
-  let SearchResultTemplate = document.querySelector("#SearchResultTemplate").content;
-
-  function showPersonsIcons(persData){
-     console.log(persData);
-     persData.forEach(function (thePers) {  
-              console.log(thePers.student_id.length);
-         const clone = SearchResultTemplate.cloneNode(true);
-         //console.log('hi clone');
-         clone.querySelector(".persIconImg").textContent = thePers.student_id;
-         //console.log('this is a loop');
-         searchResult.appendChild(clone);
-        
-    });
-}
-
-getDataResults();
-
 /******************** Fetching Skills & Cloning it to tabs *********************/
 
 "use strict"
