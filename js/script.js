0d208f55fe3c444e3608e0697ca65c965b22a6fb
Birkir Björnsson <birkirbjornsson@Birkirs-MacBook-Pro.local>
2018-12-17 13:12:50 +0100

    birkir

--- js/script.js
+++ js/script.js
@@ -215,6 +215,26 @@ $(document).ready(function(){
     if (this.hash !== "#section2") {
       // Prevent default anchor click behavior
       event.preventDefault();
+      
+
+      // Store hash
+      let hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } 
+    // End if
+    if (this.hash !== "#section3") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      
 
       // Store hash
       let hash = this.hash;
@@ -228,7 +248,7 @@ $(document).ready(function(){
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    } // End if
+    } 
   });
 });
 
